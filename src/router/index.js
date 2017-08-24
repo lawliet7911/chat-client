4f0fac0ab50fb46582c97a6a82aedbb8ed055d1f
lawliet7911 <lawliet7911@gmail.com>
2017-08-24 18:30:48 +0800

    chat-client

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import ElementUI from 'element-ui'
+import Login from '@/views/Login'
+import 'element-ui/lib/theme-default/index.css'
+
+Vue.use(Router)
+Vue.use(ElementUI)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
+    }
+  ]
+})
