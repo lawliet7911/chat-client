8ee695afaa110ed753d17e5bb8bd40bdb18df67e
lawliet7911 <lawliet7911@gmail.com>
2017-08-29 18:26:26 +0800

    Add signin page and styles

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,7 +11,7 @@
           <el-checkbox v-model="remeberPwd">记住密码</el-checkbox>
           <span @click="signin" class="signin">注册</span>
         </div>
-        <el-button v-on:click="login" class="btn_mar_t btn_login animated bounceInUp" type="info">登录</el-button>
+        <el-button v-on:click="login" class="btn_mar_t btn_login animated bounceInUp" :loading="isLoading" type="info">登录</el-button>
         <span v-if="isError" :class="errorMsgClass" class="animated">{{errorMsg}}</span>
       </form>
     </div>
@@ -19,51 +19,56 @@
 </template>
 
 <script>
-  import api from '../API/API'
-  import router from "../router/index"
-  export default {
-    created() {
-      //创建前搜索localstorag是否保存用户名密码
-      let loginInfo = localStorage.getItem("_chatroomLogin");
-      if (loginInfo) {
-        loginInfo = JSON.parse(loginInfo);
-        this.username = loginInfo.username;
-        this.pwd = loginInfo.pwd;
-        this.remeberPwd = true;
+import api from '../API/API'
+import router from "../router/index"
+export default {
+  created() {
+    //创建前搜索localstorag是否保存用户名密码
+    let loginInfo = localStorage.getItem("_chatroomLogin");
+    if (loginInfo) {
+      loginInfo = JSON.parse(loginInfo);
+      this.username = loginInfo.username;
+      this.pwd = loginInfo.pwd;
+      this.remeberPwd = true;
+    }
+  },
+  data() {
+    return {
+      username: "",
+      pwd: "",
+      isError: false,
+      errorMsg: "",
+      errorMsgClass: {
+        'shake': true,
+        'errorText': true
+      },
+      remeberPwd: false,
+      isLoading: false
+    }
+  },
+  methods: {
+    login() {
+      this.isLoading = true;
+      this.errorMsg = "";
+      this.isError = false;
+      if (!this.username) {
+        this.errorMsg = "请输入用户名！";
+        this.isError = true;
+        return;
       }
-    },
-    data() {
-      return {
-        username: "",
-        pwd: "",
-        isError: false,
-        errorMsg: "",
-        errorMsgClass: {
-          'shake': true,
-          'errorText': true
-        },
-        remeberPwd: false
+      if (!this.pwd) {
+        this.errorMsg = "请输入密码！";
+        this.isError = true;
+        return;
       }
-    },
-    methods: {
-      login() {
-        if (!this.username) {
-          this.errorMsg = "请输入用户名！";
-          this.isError = true;
-          return;
-        }
-        if (!this.pwd) {
-          this.errorMsg = "请输入密码！";
-          this.isError = true;
-          return;
-        }
+      setTimeout(() => {
         //调用接口登陆
         api.Login({
           "uname": this.username,
           "pwd": this.pwd
         }).then(res => {
-          console.log(res)
           if (!res.success) {
+            this.isLoading = false;
             this.isError = true;
             this.errorMsg = "用户名或密码错误！";
           } else {
@@ -77,91 +82,121 @@
             }
             router.push('main');
           }
+        }, err => {
+          this.errorMsg = "网络错误！";
+          this.isError = true;
+          this.isLoading = false;
         })
-      },
-      change() {
-        this.isError = false;
-      },
-      signin() {
-        router.push('signin');
-      }
+      }, 400)
+    },
+    change() {
+      this.isError = false;
+    },
+    signin() {
+      router.push('signin');
     }
   }
+}
 
 </script>
 <style scoped>
-  .signin {
-    padding-left: 25px;
-    color: deepskyblue;
-    cursor: pointer;
-  }
+.logo {
+  font-size: 4em;
+  color: #f35626;
+  background-image: -webkit-linear-gradient(92deg, #f35626, #feab3a);
+  background-clip: text;
+  -moz-background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: hue 60s infinite linear;
+  -moz-animation: hue 60s infinite linear;
+  -webkit-animation: hue 60s infinite linear;
+}
 
-  .signin:hover {
-    color: skyblue;
+@keyframes hue {
+  0% {
+    -webkit-filter: hue-rotate(0deg);
   }
-
-  .loginContainer {
-    width: 100vw;
-    height: 100vh;
-    overflow: hidden;
-    position: relative;
+  100% {
+    -webkit-filter: hue-rotate(-360deg);
   }
+}
 
-  .contain {
-    width: 450px;
-    height: 240px;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    margin: -120px 0 0 -225px;
-  }
+.signin {
+  padding-left: 25px;
+  color: deepskyblue;
+  cursor: pointer;
+}
 
-  .head {
-    display: inline-block;
-    width: 120px;
-    height: 120px;
-    border-radius: 50%;
-    border: 1px solid #20A0FF;
-    background: #20A0FF;
-    position: relative;
-    top: -80px;
-    text-align: center;
-    margin-right: 15px;
-  }
+.signin:hover {
+  color: skyblue;
+}
 
-  .head img {
-    width: 90px;
-    height: 90px;
-    display: inline-block;
-    position: relative;
-    user-select: none;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    top: 15px;
-  }
+.loginContainer {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+}
 
-  .loginPanel {
-    display: inline-block;
-    width: 300px;
-    height: 240px;
-  }
+.contain {
+  width: 450px;
+  height: 240px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin: -120px 0 0 -225px;
+}
 
-  .btn_mar_t {
-    margin-top: 20px;
-  }
+.head {
+  display: inline-block;
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  border: 1px solid #20A0FF;
+  background: #20A0FF;
+  position: relative;
+  top: -80px;
+  text-align: center;
+  margin-right: 15px;
+}
 
-  .btn_login {
-    position: relative;
-    width: 100px;
-  }
+.head img {
+  width: 90px;
+  height: 90px;
+  display: inline-block;
+  position: relative;
+  user-select: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  top: 15px;
+}
 
-  .errorText {
-    position: relative;
-    left: 50px;
-    color: #ff4949;
-    font-size: 13px;
-    display: inline-block;
-  }
+.loginPanel {
+  display: inline-block;
+  width: 300px;
+  height: 240px;
+}
 
+.btn_mar_t {
+  margin-top: 20px;
+}
+
+.btn_login {
+  position: relative;
+  width: 100px;
+}
+
+.errorText {
+  position: relative;
+  left: 50px;
+  color: #ff4949;
+  font-size: 13px;
+  display: inline-block;
+  -webkit-animation-duration: .5s;
+  animation-duration: .5s;
+  -moz-animation-duration: .5s;
+  -o-animation-duration: .5s;
+}
 </style>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,14 +1,85 @@
 <template>
-  <div>
-    注册
+  <div class="contain">
+    <div class="signinContainer animated bounceInDown">
+      <div class="title">
+        <span>新用户注册</span>
+      </div>
+      <div>
+        <form @keyup.enter="signin">
+          <el-input placeholder="用户名" type="text" @input="change" v-model="userName" class="mar_t"></el-input>
+          <el-input placeholder="昵称" type="text" @input="change" v-model="nickName" class="mar_t"></el-input>
+          <el-input placeholder="密码" type="text" @input="change" v-model="password" class="mar_t"></el-input>
+          <el-input placeholder="确认密码" type="text" @input="change" v-model="confirmPass" class="mar_t"></el-input>
+          <div class="mar_t">
+            <el-radio class="radio" v-model="sex" label="1">男</el-radio>
+            <el-radio class="radio" v-model="sex" label="2">女</el-radio>
+          </div>
+          <div class="mar_t t_c">
+            <el-button>注册</el-button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  
+  data() {
+    return {
+      sex: "1",
+      userName: "",
+      nickName: "",
+      password: "",
+      confirmPass: ""
+    }
+  },
+  methods: {
+    change() {
+
+    },
+    signin() {
+
+    }
+  }
 }
 </script>
 <style scoped>
+.contain {
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  background-color: #eaeaea;
+  position: relative;
+}
 
+.signinContainer {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  display: inline-block;
+  width: 320px;
+  height: 400px;
+  padding: 15px 30px;
+  margin: -225px 0 0 -170px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 3px 3px 10px #ccc;
+}
+.t_c{
+  text-align: center;
+}
+.mar_t {
+  margin-top: 25px;
+}
+
+.title span {
+  font-size: 17px;
+}
+
+.title hr {
+  margin-bottom: 10px;
+  color: #ccc;
+}
 </style>
 
