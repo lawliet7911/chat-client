0f9c5306d4781464257eea49923affadf0f8c8c3
lawliet7911 <lawliet7911@gmail.com>
2017-09-06 18:05:38 +0800

    1

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -9,16 +9,16 @@
       <div class="main">
         <div class="list">
           <ul>
-            <li>
+            <!-- <li>
               <span class="head"></span>
               <span class="name">贝贝❀</span>
-              <span class="message">想你了</span>
-            </li>
+              <span class="message">I miss you,where are you now?</span>
+            </li> -->
             <li>
-                <span class="head"></span>
-                <span class="name">Lawliet</span>
-                <span class="message">？怎么了</span>
-              </li>
+              <span class="head"></span>
+              <span class="name">Lawliet</span>
+              <span class="message">怎么了？</span>
+            </li>
           </ul>
         </div>
         <div class="chatArea">
@@ -29,111 +29,21 @@
   </div>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        searchUser: ""
-      }
-    },
-    methods: {
-      searchByUserName: () => {
+export default {
+  data() {
+    return {
+      searchUser: ""
+    }
+  },
+  methods: {
+    searchByUserName: () => {
 
-      }
     }
   }
+}
 
 </script>
+
 <style scoped>
-  .contain {
-    width: 100vw;
-    height: 100vh;
-    overflow: hidden;
-    background: #eaeaea;
-    position: relative;
-  }
-
-  .chatMain {
-    position: relative;
-    top: 50%;
-    margin: auto;
-    margin-top: -40vh;
-    width: 65vw;
-    height: 80vh;
-    background-color: white;
-    border-radius: 6px;
-    border: 1px solid #e1e1e1;
-    box-shadow: #ccc 5px 5px 15px;
-  }
-
-  .head {
-    border-radius: 6px 6px 0px 0px;
-    height: 75px;
-    width: 100%;
-    border-bottom: 1px solid #ccc;
-    background-color: #F2F5F7;
-  }
-
-  .head .search {
-    height: 75px;
-    line-height: 75px;
-    display: inline-block;
-    position: relative;
-    left: 120px;
-    width: 220px;
-  }
-  .main{
-    height: calc(100% - 76px);
-    widows: 100%;
-  }
-
-  .main .list {
-    float: left;
-    width: 229px;
-    height: 100%;
-    border-bottom-left-radius: 6px;
-    border-right: 1px solid #ddd;
-    overflow-x: hidden;
-    overflow-y: auto;
-  }
-
-  .main .list ul{
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-  .main .list ul li{
-    position: relative;
-    height: 74px;
-    background: #fff;
-    border-top: 1px solid #ddd;
-    border-bottom: 1px solid #ddd;
-    margin-top: -1px;
-  }
-  .main .list ul li .head{
-    position: absolute;
-    display: inline-block;
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    top: 10px;
-    left: 10px;
-    background-color: #42A5F5;
-  }
-  .main .list ul li .name{
-    position: absolute;
-    display: inline-block;
-    font-weight: 400;
-    font-size: 13pt;
-    left: 80px;
-    top: 10px;
-  }
-  .main .list ul li .message{
-    position: absolute;
-    display: inline-block;
-    font-weight: 400;
-    font-size: 10pt;
-    color: #aaa;
-    left: 80px;
-    top: 38px;
-  }
+@import './../css/main.css';
 </style>
